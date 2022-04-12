7db3ddd2ecfbc8ae30a20227dba3d990e599fe81
lov-charee <iamkaramelle@gmail.com>
2022-04-11 18:46:14 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main - function begins
  *
- * Return: Always 0 (Sucess)
+ * Return: function ends
  */
 int main(void)
 {
   puts("\"Programming is like building a multilingual puzzle");
-  return (0)
-    }
+  return (0);
+}
+  
 
+  
