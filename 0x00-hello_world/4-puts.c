954c50ff144d6c0e1c8e1a29a326bcf03dae732c
Charity Sunday <charitynkereuwemsunday@gmail.com>
2022-07-05 03:02:07 -0700

    betty problem solved

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  * main - function begins
  *
@@ -7,8 +6,7 @@
  */
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle");
-  return (0);
-}  
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 
-  
+}
